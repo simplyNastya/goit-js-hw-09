526bc1dfb01734d2d63cdc429333aee2764b8533
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-01-18 18:43:53 +0200

    Refactored code
    Update timer in task #3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,30 +2,31 @@ import Notiflix from 'notiflix';
 
 const formEl = document.querySelector('.form');
 
-let position = 1;
+let position = 0;
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  
   return new Promise((resolve, reject) => {
-    if (shouldResolve) {
-      console.log(shouldResolve)
-      resolve({position, delay})
-    } else {
-      reject({position, delay})
-    }
+    const timeoutId = setTimeout(() => {
+      if (shouldResolve) {
+      resolve({ position, delay });
+      } else {
+      reject({ position, delay });
+      }
+    }, delay)
   })
 }
 
-function handlerSubmitBtn(event) {
+function handlerSubmit(event) {
   event.preventDefault();
 
   let delay = +event.target.elements.delay.value;
   const step = +event.target.elements.step.value;
   const amount = +event.target.elements.amount.value;
 
-  let counter = 0;
-  const intervalId = setInterval(() => {
+  for (i = 0; i < amount; i += 1) {
+    position += 1
+
     createPromise(position, delay)
       .then(({ position, delay }) => {
         Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
@@ -33,15 +34,8 @@ function handlerSubmitBtn(event) {
       .catch(({ position, delay }) => {
         Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-    
-    position += 1;
-    delay += step;
-    counter += 1;
+    delay += step
+  }
+}
 
-    if (counter === amount) {
-      clearInterval(intervalId);
-    }
-  }, delay)
-}  
-
-formEl.addEventListener('submit', handlerSubmitBtn)
+formEl.addEventListener('submit', handlerSubmit)
